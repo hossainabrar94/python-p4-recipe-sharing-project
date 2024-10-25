8e1e20aa37f55dd5d42365d7b70a12503c53148a
Abrar <hossainabrar@aol.com>
2024-10-24 22:58:17 -0400

    added models

--- server/models.py
+++ server/models.py
@@ -4,3 +4,11 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    pass
+
+class Recipe(db.Model, SerializerMixin):
+    pass
+
+class Favorite(db.Model, SerializerMixin):
+    pass
