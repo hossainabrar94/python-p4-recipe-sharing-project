065cf7cfb86f83d378a1ae6f7ba40c15a226b226
Abrar <hossainabrar@aol.com>
2024-10-27 14:08:33 -0400

    added additional model

--- server/models.py
+++ server/models.py
@@ -4,11 +4,21 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+
+# One-to-Many Relationship - User has many Recipes
 class User(db.Model, SerializerMixin):
     pass
 
+
+# One-to-Many Relationship - Recipe has many Ratings
 class Recipe(db.Model, SerializerMixin):
     pass
 
+
+# Many-to-Many Relationship - User can Favorite many Recipes and Recipes can be Favorited by many Users
 class Favorite(db.Model, SerializerMixin):
+    pass
+
+
+class Rate(db.Model, SerializerMixin):
     pass
