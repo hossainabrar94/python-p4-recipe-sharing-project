fc8328d99bb6e95a04caaa05f291fd279b156eb8
Abrar <hossainabrar@aol.com>
2024-10-27 03:20:35 -0400

    added backend app views

--- server/app.py
+++ server/app.py
@@ -9,9 +9,46 @@ from flask_restful import Resource
 # Local imports
 from config import app, db, api
 # Add your model imports
+from models import User, Recipe, Favorite
 
 
 # Views go here!
+class Signup(Resource):
+    pass
+
+class Login(Resource):
+    pass
+
+class Logout(Resource):
+    pass
+
+class RecipeList(Resource):
+    pass
+
+class RecipeDetail(Resource):
+    pass
+
+class Favorite(Resource):
+    pass
+
+class Rating(Resource):
+    pass
+
+class New_Recipe(Resource):
+    pass
+
+class CheckSession(Resource):
+    pass
+
+
+api.add_resource(Signup, '/signup', endpoint='signup')
+api.add_resource(Login, '/login', endpoint='login')
+api.add_resource(Logout, '/logout', endpoint='logout')
+api.add_resource(Favorite, '/favorites', endpoint='favorites')
+api.add_resource(RecipeList, '/recipes', endpoint='recipes')
+api.add_resource(RecipeDetail, '/recipes/<int:id>', endpoint='recipes_detail')
+api.add_resource(Rating, '/recipes/rating', endpoint='rated_recipes')
+api.add_resource(CheckSession, '/check_session', endpoint='check_session')
 
 @app.route('/')
 def index():
